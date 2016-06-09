57f6b676411b70d345ede96915e758a100fbb4a8
dtolhuijs <deisi.bonilla@gmail.com>
2016-06-09 10:39:38 +0200

    new contact

--- tacta.rb
+++ tacta.rb
@@ -13,6 +13,19 @@ def show(contact)
   puts "email: #{contact[:email]}"
 end
 
+def create_new
+   contact = {}
+
+   puts
+   puts "Enter contact info:"
+
+   contact[:name ] = ask "Name? "
+   contact[:phone] = ask "Phone? "
+   contact[:email] = ask "Email? "
+
+   contact
+end
+
 def ask(prompt)
    puts
    print prompt
@@ -28,16 +41,33 @@ contacts << { name: "Genghis Khan"    , phone: "+976 2 194 2222" , email: "conta
 contacts << { name: "Malcom X"        , phone: "+1 310 155 8822" , email: "x@theroost.org"     }
 
 loop do
-  index(contacts)
+   index( contacts )
 
-  puts
-  response = ask("Who would you like to see? (q to quit)")
-  break if response == "q"
+   puts
+   response = ask "Who would you like to see (n for new, q to quit)? "
 
-  i = response.to_i
+   break if response == "q"
 
-  contact = contacts[i-1]
+     if response == "n"
 
-  show(contact)
-  puts
+     contact = create_new
+
+     contacts << contact
+
+     puts
+     puts "New contact created:"
+     puts
+
+     show( contact )
+     puts
+        else
+      i = response.to_i
+
+      contact = contacts[i-1]
+
+      puts
+      show( contact )
+      puts
+   end
+
 end
